6b5ca4156a304baca4dab6d0e3176580abb13246
Jakeokony1024 <jakeokony@gmail.com>
2020-03-24 13:18:12 -0500

    db con. good, formatted css pages

--- config/connection.js
+++ config/connection.js
@@ -14,3 +14,26 @@ var sequelize = new Sequelize("coreysFitnessdb", "root", "tomsucks12", {
 
 // Exports the connection for other files to use
 module.exports = sequelize;
+
+// setting up the mysql connection
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "tomsucks12",
+//   database: "coreysFitnessdb"
+// });
+
+// // make a connection
+
+// connection.connect(function(error) {
+//   if (error) {
+//     console.log("error connecting: " + error.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
