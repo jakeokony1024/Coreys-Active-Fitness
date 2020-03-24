596f0d29b24fe71bc99df74930e90f578f9837f3
Jakeokony1024 <jakeokony@gmail.com>
2020-03-24 14:21:05 -0500

    moved img,fonts,js folders to public

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,33 +5,6 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.User.findAll({}).then(function(dbUser) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbUser
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
-      res.render("example", {
-        example: dbUser
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
-var db = require("../models");
-
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(dbUser) {
-      res.render("index", {
         style: "style.css",
         msg: "Welcome!",
         examples: dbUser
@@ -79,7 +52,6 @@ module.exports = function(app) {
       });
     });
   });
-
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
